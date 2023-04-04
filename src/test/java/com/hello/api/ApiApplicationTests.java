b36f5abb79b80e7944556cf2672acc96b80cfb5d
Victor Vermaas <vhvermaas@gmail.com>
2023-04-04 11:17:28 -0300

    initialized

---
+++ src/test/java/com/hello/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hello.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
